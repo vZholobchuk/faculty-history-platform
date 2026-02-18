12e8f551fa20e43acc332994cf2373a8641ecfb9
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2026-02-18 22:00:42 +0200

    Add pagination

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, url_for
 from flask_cors import CORS
 from models import db, Event, EventPhoto,Person,Document
 import os
 from werkzeug.utils import secure_filename
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
-from .constants import CATEGORIES
+from constants import CATEGORIES, FILE_TYPES
 
 app = Flask(
     __name__,
@@ -89,7 +89,74 @@ def admin():
 
 @app.route('/archive')
 def archive():
-    return render_template('archive.html')
+    archive=[
+        {
+            "title": "Наказ про заснування факультету",
+            "subtitle": "Оригінальний відсканований примірник",
+            "category": "leadership",
+            "year": 1965,
+            "file_type": "pdf",
+            "file_url": url_for("static", filename="html/text.pdf")
+        },
+        {
+            "title": "Протокол Вченої ради №12",
+            "subtitle": "Обговорення нових програм навчання",
+            "category": "science",
+            "year": 1982,
+            "file_type": "docx",
+            "file_url": url_for("static", filename="html/text.docx")
+        },
+        {
+            "title": "Перший диплом випускника ПНУ",
+            "subtitle": "Архівний зразок диплома спеціаліста",
+            "category": "students",
+            "year": 1970,
+            "file_type": "jpg",
+            "file_url": url_for("static", filename="html/text.jpg")
+        }
+    ]
+
+    query = request.args.get("search", "").lower()
+    category = request.args.get("category", "")
+    year = request.args.get("year", type=int)
+    page = request.args.get("page", 1, type=int)
+    per_page = 2
+
+    filtered_archive = []
+
+    for item in archive:
+        if query and query not in item["title"].lower() and query not in item["subtitle"].lower():
+            continue
+
+        if category and category != item["category"]:
+            continue
+
+        if year:
+            try:
+                if int(year) != item["year"]:
+                    continue
+            except ValueError:
+                pass
+        filtered_archive.append(item)
+            
+    total_items = len(filtered_archive)
+    total_pages = (total_items + per_page - 1) // per_page
+
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_archive = filtered_archive[start:end]
+
+    return render_template('archive.html', 
+                    CATEGORIES=CATEGORIES, 
+                    FILE_TYPES=FILE_TYPES, 
+                    archive=paginated_archive,
+                    query=query,
+                    selected_year=year,
+                    selected_category=category,
+                    current_page=page,
+                    per_page=per_page,
+                    total_pages=total_pages
+    )
 
 # Оновлений ендпоінт з фільтрацією
 @app.route('/api/events', methods=['GET'])
--- backend/constants.py
+++ backend/constants.py
@@ -36,3 +36,106 @@ CATEGORIES = {
         "icon": "bi-people-fill",
     }
 }
+
+FILE_TYPES = {
+    "pdf": {
+        "icon": "bi-file-earmark-pdf-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "docx": {
+        "icon": "bi-file-earmark-word-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "doc": {
+        "icon": "bi-file-earmark-word-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "xls": {
+        "icon": "bi-file-earmark-excel-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "xlsx": {
+        "icon": "bi-file-earmark-excel-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "ppt": {
+        "icon": "bi-file-earmark-ppt-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "pptx": {
+        "icon": "bi-file-earmark-ppt-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "txt": {
+        "icon": "bi-file-earmark-text",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "csv": {
+        "icon": "bi-file-earmark-spreadsheet-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "jpg": {
+        "icon": "bi-file-earmark-image-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "jpeg": {
+        "icon": "bi-file-earmark-image-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "png": {
+        "icon": "bi-file-earmark-image-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "gif": {
+        "icon": "bi-file-earmark-image-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "mp3": {
+        "icon": "bi-file-earmark-music-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "wav": {
+        "icon": "bi-file-earmark-music-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "mp4": {
+        "icon": "bi-file-earmark-play-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "mov": {
+        "icon": "bi-file-earmark-play-fill",
+        "btn_title": "Перегляд",
+        "btn_icon": "bi-eye"
+    },
+    "zip": {
+        "icon": "bi-file-earmark-zip-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "rar": {
+        "icon": "bi-file-earmark-zip-fill",
+        "btn_title": "Завантажити",
+        "btn_icon": "bi-download"
+    },
+    "default": {
+        "icon": "bi-file-earmark-fill",
+        "btn_title": "Відкрити",
+        "btn_icon": "bi-eye"
+    }
+}
